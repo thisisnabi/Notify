ccfcc48dc7d3dcec79ee896dd23ceb7cc8659af7
Nabi Karampour <N.Karampour@Alibaba.ir>
2024-05-31 14:13:04 +0330

    Clean up web application

--- src/Notify.Hosts.WebApp/Program.cs
+++ src/Notify.Hosts.WebApp/Program.cs
@@ -1,15 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -19,7 +14,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
